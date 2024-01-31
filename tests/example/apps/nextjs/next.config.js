cbd87032165671369be4482bbeaedb38289b5476
Dario Lehmhus <dario.lehmhus@ffwagency.com>
2024-01-31 14:15:44 +0100

    chore: update dependencies

--- tests/example/apps/nextjs/next.config.js
+++ tests/example/apps/nextjs/next.config.js
@@ -3,5 +3,8 @@ const withLinaria = require('../../../../');
 /** @type {import('../../../../').LinariaConfig} */
 const config = {
   transpilePackages: ['ui-kit'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 module.exports = withLinaria(config);
