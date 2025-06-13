e52df493f5934020165a810e567304c8a2fdf17b
Dario Lehmhus <27899554+dlehmhus@users.noreply.github.com>
2025-06-13 15:49:46 +0200

    v1.2.0 (#40)

--- src/add-turbopack-config.ts
+++ src/add-turbopack-config.ts
@@ -8,28 +8,46 @@ export function addTurbopackConfig({
 }: WithLinariaConfig) {
   config.turbopack ??= {};
   config.turbopack.rules ??= {};
-  config.turbopack.rules = {
-    ...config.turbopack.rules,
-    // @ts-expect-error Turbopack only allows for primitive options that can be serialized to JSON
-    // but the default linaria options include functions. We ignore this for now.
-    '*.{ts,tsx,js,jsx}': {
-      loaders: [
-        {
-          loader: path.resolve(
-            __dirname,
-            './loaders/turbopack-transform-loader',
-          ),
-          options: {
-            sourceMap: process.env.NODE_ENV !== 'production',
-            displayName: process.env.NODE_ENV !== 'production',
-            ...linariaOptions,
-            babelOptions: {
-              presets: ['next/babel', '@wyw-in-js'],
-            },
-          },
-        },
-      ],
+
+  const linariaLoader = {
+    loader: path.resolve(__dirname, './loaders/turbopack-transform-loader'),
+    options: {
+      sourceMap: process.env.NODE_ENV !== 'production',
+      displayName: process.env.NODE_ENV !== 'production',
+      ...linariaOptions,
+      babelOptions: {
+        presets: ['next/babel', '@wyw-in-js'],
+      },
     },
+  };
+
+  // TODO: re-enable this once https://github.com/vercel/next.js/issues/79592 is fixed
+  // and remove the regex approach below
+
+  // if (config.turbopack.rules['*.{ts,tsx,js,jsx}']) {
+  //   // @ts-expect-error Turbopack only allows for primitive options that can be serialized to JSON
+  //   // but the default linaria options include functions. We ignore this for now.
+  //   config.turbopack.rules['*.{ts,tsx,js,jsx}'].loaders.push(linariaLoader);
+  // } else {
+  //   // @ts-expect-error Turbopack only allows for primitive options that can be serialized to JSON
+  //   config.turbopack.rules['*.{ts,tsx,js,jsx}'] = {
+  //     loaders: [linariaLoader],
+  //   };
+  // }
+
+  const conditionId = '#linaria-loader';
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  config.turbopack.conditions ??= {};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  config.turbopack.conditions[conditionId] = {
+    path: /^(?!.*middleware\.(tsx?|jsx?)$).*\.(tsx?|jsx?)$/,
+  };
+
+  // @ts-expect-error Turbopack only allows for primitive options that can be serialized to JSON
+  // but the default linaria options include functions. We ignore this for now.
+  config.turbopack.rules[conditionId] = {
+    loaders: [linariaLoader],
   };
 
   return config;
